f4eaa3773463219a12913d0a5747c9ad0ce29632
shavit <shavit@users.noreply.github.com>
2022-09-27 11:24:00 -0400

    start program

--- src/ast.zig
+++ src/ast.zig
@@ -1,8 +1,60 @@
-const Statement = struct {
+const std = @import("std");
+const testing = std.testing;
+const mem = std.mem;
+const Token = @import("token").Token;
+
+pub const StatementType = enum {
+    let,
+};
+
+pub const Statement = struct {
+    const Self = @This();
+    t: StatementType,
     literal: []const u8,
     str: []const u8,
+
+    fn toString(self: *Self) []const u8 {
+        return switch (self) {
+            else => letToString(),
+        };
+    }
+
+    fn letToString() []const u8 {
+        return "";
+    }
 };
 
 pub const Program = struct {
+    const Self = @This();
     statements: []const Statement,
+
+    fn init() Self {
+        return Self{ .statements = &[_]Statement{} };
+    }
+
+    fn toString(self: *Self) []const u8 {
+        //var buf: [1024 * 16]u8 = undefined;
+        if (self.statements.len > 2) {
+            return "2";
+        }
+
+        return "";
+    }
 };
+
+const Identifier = struct {
+    const Self = @This();
+    token: Token,
+    value: []const u8,
+
+    fn tokenLiteral(self: *Self) []const u8 {
+        return self.token.literal;
+    }
+};
+
+test "ast statements" {
+    var program = Program.init();
+    const expected = "";
+
+    try testing.expect(mem.eql(u8, expected, program.toString()));
+}
--- src/parser.zig
+++ src/parser.zig
@@ -3,10 +3,16 @@ const testing = std.testing;
 
 const lexer = @import("lexer");
 const Lexer = lexer.Lexer;
+const token = @import("token");
+const Token = token.Token;
+const TokenType = token.TokenType;
+const ast = @import("ast");
 
 pub const Parser = struct {
     const Self = @This();
     lex: Lexer,
+    tok_curr: Token,
+    tok_peek: Token,
     errors: []const []const u8,
     prefix_parse_fns: []u8,
     infix_parse_fns: []u8,
@@ -19,4 +25,44 @@ pub const Parser = struct {
             .infix_parse_fns = .{},
         };
     }
+
+    fn next_token(self: *Self) void {
+        self.tok_curr = self.tok_peek;
+        self.tok_peek = self.lex.next_token();
+    }
+
+    fn parse(self: *Self) ast.Program {
+        const program = ast.Program{};
+        while (self.tok_curr.T != TokenType.eof) {
+            const stmt = self.parse_statement();
+            if (stmt != null) {}
+            self.next_token();
+        }
+        return program;
+    }
+
+    fn parse_statement(self: *Self) ?ast.Statement {
+        if (self.tok_curr.T == TokenType.eof) {
+            return null;
+        }
+        return null;
+    }
 };
+
+test "return statements" {
+    const TestCase = struct {
+        input: []const u8,
+        num_stmt: u8,
+    };
+    const testCases = [_]TestCase{
+        .{ .input = "return 1; return 2; return 3;", .num_stmt = 3 },
+    };
+
+    for (testCases) |t| {
+        const l = Lexer.init(t.input);
+        const p = Parser.init(l);
+        const program = p.parse();
+
+        try testing.expect(program.statements.len == t.num_stmt);
+    }
+}
